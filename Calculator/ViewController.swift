186b1b8e3bf5fd3ffdbe77bdd94cfd6624286869
Farzin Faghihi <farzin@farzins-macbook-pro.local>
2017-03-07 19:46:55 -0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Farzin Faghihi on 2017-03-07.
+//  Copyright © 2017 Farzin Faghihi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
